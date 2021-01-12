8fbd08b79bfd525e56d1eec27e7c560a00752ecc
AznivSaroyan <aznivsaroyan87@gmail.com>
2021-01-12 14:28:42 +0400

    first commit

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const {Schema, model, Types}  = require('mongoose')
+
+const schema = new Schema({
+  email: {
+    type: String, required: true, unique: true
+  },
+  password: {
+    type: String, required: true
+  },
+  post: [{
+    type: Types.ObjectId, ref: 'Post'
+  }]
+})
+
+module.exports = model('User', schema)
